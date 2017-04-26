cd53e65a1fd1454753fa0f3624be4cf8a3068464
LeeZFU <lee0316zf@163.com>
2017-04-26 16:37:47 +0800

    work_3

---
+++ IntentReceiver/app/src/main/java/com/intentreceiver/OtherActivity.java
@@ -0,0 +1,20 @@
+package com.intentreceiver;
+
+/**
+ * Created by LZF on 2017/4/26.
+ */
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class OtherActivity extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        //从Intent当中根据key取得value
+        String value = intent.getStringExtra("testIntent");
+        Log.e("IntentReceiver-->Test", value);
+    }
+
+}
